e38db3bc740aeebaab737d29d4f22de638b76d12
Byeong Rok <rokany@cau.ac.kr>
2022-11-12 17:19:40 +0900

    step1 complete

--- Gwacheon/ContentView.swift
+++ Gwacheon/ContentView.swift
@@ -9,18 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading){
-            Text("Gwanaksan Valley")
-                .font(.title)
-            HStack{
-                Text("Gwcheon Jungang-dong")
-                    .font(.subheadline)
-                Spacer()
-                Text("Gyeonggi, South Korea")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("관악산 계곡")
+                    .font(.title)
+                
+                HStack{
+                    Text("과천시 중앙동")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("경기도, 대한민국")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                
+                Text("관악산 계곡은?")
+                    .font(.title2)
+                Text("과천 향교 옆에 자리한 시원한 계곡")
+
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
