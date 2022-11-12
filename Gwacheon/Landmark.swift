9c27aa7fae347f2a2224e42c8c85d3b0c3211970
Byeong Rok <rokany@cau.ac.kr>
2022-11-12 23:50:30 +0900

    add Landmark view

--- Gwacheon/Landmark.swift
+++ Gwacheon/Landmark.swift
@@ -1,10 +1,16 @@
 import Foundation
+import SwiftUI
 
-struct Landmark : Hashable, Codable //makes it easier to move data between the structure and a data file {
+struct Landmark : Hashable, Codable { //makes it easier to move data between the structure and a data file
     //landmarkData.json 속 key 자료형에 맞춰준다.
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
+    
+    private var imageName : String
+    var image : Image{
+        Image(imageName)
+    }
 }
